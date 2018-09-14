b979a794d05b1bf5412653f191932ece9c59fc76
Fellipe Amorim <fellipedf@gmail.com>
2018-09-13 22:17:24 -0300

    Implementacao do CORS.

--- src/main/java/com/vistorieja/emailws/EmailWsApplication.java
+++ src/main/java/com/vistorieja/emailws/EmailWsApplication.java
@@ -2,6 +2,12 @@ package com.vistorieja.emailws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmailWsApplication {
@@ -9,4 +15,5 @@ public class EmailWsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmailWsApplication.class, args);
 	}
+
 }
